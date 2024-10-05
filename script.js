0aed340c616c2b843cea047c6e40a49a4733f745
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2024-10-05 20:42:45 +0200

    renderLiked function implemented

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 function renderBooks() {
-  let fullContentRef = document.getElementById('full_content');
+  let fullContentRef = document.getElementById("full_content");
   fullContentRef.innerHTML = "";
   for (let indexBook = 0; indexBook < books.length; indexBook++) {
-      fullContentRef.innerHTML += getBooksHTMLTemplate(indexBook);
-      renderComments(indexBook);
+    fullContentRef.innerHTML += getBooksHTMLTemplate(indexBook);
+    renderliked(indexBook);
+    renderComments(indexBook);
   }
 }
 
 function renderComments(indexBook) {
-  let bookCommentsRef = document.getElementById(`book_comments${(indexBook)}`);
+  let bookCommentsRef = document.getElementById(`book_comments${indexBook}`);
   if (books[indexBook]["comments"].length == 0) {
-    return
+    return;
   }
 
-  bookCommentsRef.innerHTML = ""
-  for (let indexBookComment = 0; indexBookComment < books[indexBook]["comments"].length; indexBookComment++) {
-    bookCommentsRef.innerHTML += getBookCommentsHTMLTemplate(indexBook, indexBookComment);
+  bookCommentsRef.innerHTML = "";
+  for (
+    let indexBookComment = 0;
+    indexBookComment < books[indexBook]["comments"].length;
+    indexBookComment++
+  ) {
+    bookCommentsRef.innerHTML += getBookCommentsHTMLTemplate(
+      indexBook,
+      indexBookComment
+    );
+  }
+}
+
+function renderliked(indexBook) {
+  let bookAlreadyLiked = "";
+  bookAlreadyLiked = books[indexBook]["liked"];
+  let bookLikedRef = document.getElementById(`book_likes_heart_${(indexBook)}`);
+
+  if (bookAlreadyLiked) {
+    console.log('bookAlreadyLiked = True')
+  } else {
+    console.log('bookAlreadyLiked = False')
+    bookLikedRef.classList.add("book_not_liked");
   }
 }
